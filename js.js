69aac6061077105a1a920af24bf4f83490124752
findthednif <130528364+findthednif@users.noreply.github.com>
2023-07-11 17:53:33 +0500

    Update js.js

--- js.js
+++ js.js
@@ -2,26 +2,47 @@ const searchLine = document.querySelector(".input__searchLine");
 const autocomplite = document.querySelector(".input__autocompleete");
 const repoList = document.querySelector('.app__repoList');
 let repositories;
-let timeOutId;
-searchLine.addEventListener("keyup", async (event) => {
-  clearTimeout(timeOutId);
-  timeOutId = setTimeout(async () => {
-    if (event.target.value.trim() === "") {
-      autocomplite.textContent = "";
-      autocomplite.classList.remove('input__autocompleete--active');
-      return;
+function debounce (fn, debounceTime) {
+  let delay;
+  return function(...args){
+    clearTimeout(delay);
+    delay = setTimeout(()=>{
+      fn.apply(this, args)
+    }, debounceTime)
+  }
+};
+async function getRepositories(key) {
+  try {
+    let response = await fetch(`https://api.github.com/search/repositories?q=${key}`);
+    if (!response.ok) {
+      throw new Error(`Server response failed. Error status: ${response.status}`);
     }
-    const response = await fetch(`https://api.github.com/search/repositories?q=${event.target.value}`);
-    if (response.status === 200) {
-      console.log('response received');
-      autocomplite.classList.add('input__autocompleete--active');
-      repositories = await response.json();
-      autocomplite.textContent = "";
-      repositories.items.slice(0, 5).forEach(repo => {
-        autocomplite.insertAdjacentHTML("beforeend", `<li class="autocompleete__elem">${repo.name}</li>`);
-      });
-    }
-  }, 500);
+    return await response.json();
+  } catch (error) {
+    throw new Error(`Failed to fetch repositories: ${error.message}`);
+  }
+}
+async function createAutoComplite(searchValue){
+  {
+    autocomplite.classList.add('input__autocompleete--active');
+    repositories = await getRepositories(searchValue);
+    autocomplite.textContent = "";
+    repositories.items.slice(0, 5).forEach(repo => {
+      autocomplite.insertAdjacentHTML("beforeend", `<li class="autocompleete__elem">${repo.name}</li>`);
+    });
+  }
+}
+const debouncedAutoComplete = debounce(async (value) => {
+  if (value.trim() === "") {
+    autocomplite.textContent = "";
+    autocomplite.classList.remove('input__autocompleete--active');
+    return;
+  }
+  await createAutoComplite(value);
+}, 500);
+
+searchLine.addEventListener("keyup", (event) => {
+  debouncedAutoComplete(event.target.value);
 });
 autocomplite.addEventListener('click', (event) => {
   if (event.target.classList.contains('autocompleete__elem')) {
@@ -29,10 +50,11 @@ autocomplite.addEventListener('click', (event) => {
     autocomplite.classList.remove('input__autocompleete--active');
     searchLine.value = '';
     const selectedRepoName = event.target.textContent;
+    const repoData = repositories.items.find(repo => repo.name === selectedRepoName);
     repoList.insertAdjacentHTML('beforeend', `<li class="repoList__elem">
       <div>Name: ${selectedRepoName}</div>
-      <div>Owner: ${repositories.items.find(repo => repo.name === selectedRepoName).owner.login}</div>
-      <div>Stars: ${repositories.items.find(repo => repo.name === selectedRepoName).stargazers_count}</div>
+      <div>Owner: ${repoData.owner.login}</div>
+      <div>Stars: ${repoData.stargazers_count}</div>
       <button class="removeBtn">Delete</button>
     </li>`);
   }
